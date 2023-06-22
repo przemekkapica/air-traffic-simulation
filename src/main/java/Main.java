4b01705885a5268f9e3943ee1d6b34356691c75c
przemekkapica <przemekapica@gmail.com>
2023-06-22 13:45:21 -0700

    Pass simulation number to runScenario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,18 @@ import util.ScenarioEngine;
 
 import java.io.FileNotFoundException;
 
-public class AirportManagementSimulation {
+
+
+public class Main {
+    private static final int simulationNumber = 0;
+
     // this is the main jade entry point function
     // it can be used to set up the platform, there is no need to use any additional agents for setup
     private static void jadeThread () {
 
         try {
             ScenarioEngine scenarioEngine = new ScenarioEngine();
-            scenarioEngine.runScenario(0);
+            scenarioEngine.runScenario(simulationNumber);
         } catch (FileNotFoundException | StaleProxyException e) {
             throw new RuntimeException(e);
         }
@@ -20,11 +24,19 @@ public class AirportManagementSimulation {
 
     private static void loadSimulation () {
         Simulation.restartScene ();
-        ScenarioManager.RunScenario0();
-//       ScenarioRunner.RunScenario1(); //scenario 1 overview of messaging used for simulating movement
-//       ScenarioRunner.RunScenario2(); //scenario 2 showcase negotiation between agents regarding speed
-//       ScenarioRunner.RunScenario3(); // scenario 3 and 4. Comparison of pathfinding algorithm for cost and distance
-//       ScenarioRunner.RunScenario4(); // scenario 5 and 6. Comparison of pathfinding algorithm for load and distance
+        if (simulationNumber == 0) {
+            ScenarioManager.RunScenario0();
+        } else if (simulationNumber == 1) {
+            ScenarioManager.RunScenario1();
+        } else if (simulationNumber == 2) {
+            ScenarioManager.RunScenario2();
+        } else if (simulationNumber == 3) {
+            ScenarioManager.RunScenario3();
+        } else if (simulationNumber == 4) {
+            ScenarioManager.RunScenario4();
+        } else {
+          throw new RuntimeException("Given simulation does not exist");
+        }
     }
 
     public static void main (String[] args) {
@@ -32,7 +44,7 @@ public class AirportManagementSimulation {
         // create a thread to run jade in
         // we want to keep the main thread for our simulation rendering
         // this is because glfw does not behave well when it is run in non-main thread apparently
-        Thread agentPlatformThread = new Thread (AirportManagementSimulation::jadeThread);
+        Thread agentPlatformThread = new Thread (Main::jadeThread);
         agentPlatformThread.start();
 
         // now, we want to start the simulation
